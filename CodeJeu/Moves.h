8450b4951e468bd53bb68bd31423cc1d9246f939
Maud Buffier <maud.buffier@outlook.com>
2016-01-30 19:48:55 +0100

    rien

---
+++ CodeJeu/Moves.h
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <iostream>
+#pragma once
+#include <vector>
+#include "Vec3.h"
+#include "Mesh.h"
+
+class Moves {
+
+  public :
+    int i ;
+    int j ;
+
+  public :
+    void move(Mesh _mesh) ;
+    vector<Int2> FindCenter(vector<Int2>) ;
+    
+
+
+} ;
